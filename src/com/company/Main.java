7e86ada766c79bc44ed7a59bc38279bf80116891
Carlos <49226878+Preda7or0fSouls@users.noreply.github.com>
2022-02-09 12:37:40 +0100

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,64 +1,89 @@
 package com.company;
 
 import java.util.Scanner;
+/*@author Carlos Soler
+ *
+ */
 
-public class Main {
+
+public class Main implements generarPrimos {
 
 	public static void main(String[] args) {
 
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("Introduce el número para la criba de Erastótenes:");
-		int dato = teclado.nextInt();
-		int vector[] = new int[dato];
-		System.out.println("\nVector inicial hasta :" + dato);
-		for (int i = 0; i < vector.length; i++) {
+		int x = teclado.nextInt();
+		int array[] = new int[x];
+
+		System.out.println("\nVector inicial hasta :" + x);
+		for (int i = 0; i < array.length; i++) {
 			if (i % 10 == 0)
 				System.out.println();
 			System.out.print(i + 1 + "\t");
 		}
-		vector = generarPrimos(dato);
-		System.out.println("\nVector de primos hasta:" + dato);
-		for (int i = 0; i < vector.length; i++) {
+		array = generarPrimos(x);
+		System.out.println("\nVector de primos hasta:" + x);
+		for (int i = 0; i < array.length; i++) {
 			if (i % 10 == 0)
 				System.out.println();
-			System.out.print(vector[i] + "\t");
+			System.out.print(array[i] + "\t");
 		}
 	}
 
+	/**
+	 * Generar primos int [ ].
+	 *
+	 * @param max the max
+	 * @return the int [ ]
+	 */
 // Generar números primos de 1 a max
 	public static int[] generarPrimos (int max) {
-            int i,j;
-            if (max >= 2) {
+		int i, j;
+		int dim = 0;
+		if (max >= 2) {
 // Declaraciones
-                int dim = max + 1; // Tamaño del array boolean[] esPrimo = new boolean[dim];
-				// Inicializar el array
-                for (i=0; i<dim; i++)
-                    esPrimo[i] = true;
+			dim = max + 1;
+			boolean[] esPrimo = new boolean[dim];
+			// Inicializar el array
+			for (i = 0; i < dim; i++)
+				esPrimo[i] = true;
 // Eliminar el 0 y el 1, que no son primos
-    }
-            esPrimo[0] = esPrimo[1] = false;
+		}
+		boolean[] esPrimo;
+		esPrimo[0] && esPrimo[1] == false;
 // Criba
-            for (i=2; i<Math.sqrt(dim)+1; i++) {
-                if (esPrimo[i]) {
-// Eliminar los múltiplos de i for (j=2*i; j<dim; j+=i)
-                    esPrimo[j] = false;
-                }
+		boolean[] Primo;
+		for (i = 2; i < Math.sqrt(dim) + 1; i++) {
+			if (Primo[i]) {
+// Eliminar los múltiplos de i
+				for (j = 2 * i; j < dim; j += i)
+					Primo[j] = false;
 			}
+		}
 // ¿Cuántos primos hay?
-            int cuenta = 0;
-            for (i=0; i<dim; i++) {
-                if (esPrimo[i])
-                    cuenta++;
-            }
-            // Rellenar el vector de números primos
-            int[] primos = new int[cuenta];
-			for (i=0, j=0; i<dim; i++) {
-                if (esPrimo[i]) primos[j++] = i;
-            }
-            return primos;
-    }else{// max < 2
-		return new int[0] ;
+		int acc = 0;
+		for (i = 0; i < dim; i++) {
+			if (Primo[i])
+				acc++;
+		}
+		// Rellenar el vector de números primos
+		int[] primos = new int[acc];
+		for (i = 0, j = 0; i < dim; i++) {
+			if (Primo[i]) primos[j++] = i;
+		}
+		return primos;
+	}else{// max < 2
+		return new int[0];
 	// Vector vacío
 		}
 	}
-}
+
+/*CAMBIOS EFECTUADOS:
+-Cambiar la variable dato renombrandola como x
+-Cambiado la variable cuenta renombrandola como acc
+-Modificamos esPrimo[0] = esPrimo[1] = false; y  dejar esPrimo[0] && esPrimo[1] == false;
+-Cambiada la variable booleana esprimo por primo
+-Cambiadada la variable vector por array
+-Eliminada la sentencia int array[] = new int[x] por repeticion y aliteracion del codigo.
+-Se a extraido el metodo que genera los primos
+ */
